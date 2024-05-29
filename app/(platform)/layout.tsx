314cf076ac30351c56c233dc8d97a32639a0fa43
dafi <oxelolol@gmail.com>
2024-05-29 17:53:44 +0300

    feat: добавит публичный ключ в clerk

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -3,8 +3,9 @@ import { ReactNode } from "react";
 import { Toaster } from 'sonner'
 
 const PlatformLayout = ({children}: {children: ReactNode}) => {
+
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <Toaster/>
       {children}
     </ClerkProvider>
