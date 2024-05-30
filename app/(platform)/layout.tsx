7a3f44934f8438fae33ab69ba206046bdbafe23b
dafi <oxelolol@gmail.com>
2024-05-30 12:53:17 +0300

    fix: перенести clerkProvider в рутовый layout

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,14 +1,13 @@
-import { ClerkProvider } from "@clerk/nextjs";
 import { ReactNode } from "react";
 import { Toaster } from 'sonner'
 
 const PlatformLayout = ({children}: {children: ReactNode}) => {
 
   return (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <div>
       <Toaster/>
       {children}
-    </ClerkProvider>
+    </div>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
 import { siteConfig } from "@/config/siteConfig";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,8 +26,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
